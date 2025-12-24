dd65af36d00dbc81bcbab64c74fb88b987fbb389
Nikita Grinyov <nikita3grynov@gmail.com>
2025-12-24 16:54:30 +0200

    home url

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -18,7 +18,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from projects.views import ProjectListView
+
 urlpatterns = [
+    path("", ProjectListView.as_view(), name="home"),
     path("admin/", admin.site.urls),
     path("users/", include("users.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
